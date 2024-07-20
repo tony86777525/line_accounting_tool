d32f7d65941b3cf9a273600c2c4180ae97976013
DC130 <tony86777525@gmail.com>
2024-07-20 21:11:06 +0800

    add line

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    const liffID = '2005889130-qp2MMZPk';
     const gas = 'https://script.google.com/macros/s/AKfycbyij5xtv_l_l4jS0mWTju6ddB-AOHkQOsH-xGYChobJoick3wAQQSnw5pxFwrsgY5oJ/exec'
     $(document).ready(() => {
         const today = new Date().toISOString().substr(0, 10);
@@ -8,6 +9,24 @@ $(document).ready(function() {
         let categories = ["飲食", "交通", "娛樂", "醫療", "保險", "教育", "衣服", "旅遊", "房貸", "水電", "其他"];
         $("#category").autocomplete({
             source: categories
+        });
+
+        let isLoggedIn = false;
+        liff.init({
+            liffId: liffID
+        }).then(function() {
+            console.log('LIFF init');
+
+            const isLoggedIn = liff.isLoggedIn();
+
+            if(isLoggedIn) {
+                liff.getProfile().then(profile => {
+                    $('#user').val(profile.displayName);
+                })
+            }
+
+        }).catch(function(error) {
+            console.log(error);
         });
     });
     $('#accountingForm').on('submit', function(event) {
